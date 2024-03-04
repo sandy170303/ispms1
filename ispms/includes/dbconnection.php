ec327c216908f2ea9b974624bbc73a7717d562c3
sandy170303 <154214673+sandy170303@users.noreply.github.com>
2024-03-04 11:56:17 +0530

    Update dbconnection.php

--- ispms/includes/dbconnection.php
+++ ispms/includes/dbconnection.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','Test@123');
 define('DB_NAME','ispmsdb');
 // Establish database connection.
 try
